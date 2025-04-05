daa929b88830efd996e8215ebaa8cf9338f22517
Vincent Ugwueze <ugwuezev@gmail.com>
2025-04-05 01:31:15 +0100

    add code for reading service account key

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const admin = require('firebase-admin');
+
 admin.initializeApp({
-  credential: admin.credential.cert(require('./serviceAccountKey.json'))
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+  }),
 });
 
 const db = admin.firestore();
